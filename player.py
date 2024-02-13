4ead5c5689a75b6d6a40b445da4c01e1b2c71031
Hannah Kearl <hannah@Hannahs-MacBook-Air.local>
2024-02-13 11:29:40 -0700

    also renamed my file to player.py

--- player.py
+++ player.py
@@ -18,3 +18,35 @@ class Player:
             return "Your ability is enhanced intellect! +2 to any percesption roll."
         else:
             return "No age entered, please try again."
+from player import Player
+from monster import Monster
+from weapon import Weapon
+
+from abc import ABC, abstractmethod
+
+class Monster(ABC):
+    def __init__(self, name: str, damage: int, health: int, ) -> None:
+        self.damage = damage
+        self.health = health
+        self.name = name
+
+
+    @abstractmethod
+    def monster_ability(self, name: str, damage: int) -> None:
+        pass
+
+
+class Goblin(Monster): pass
+
+class Orc(Monster): pass 
+
+class DireWolf(Monster): pass 
+
+class Dragon(Monster): pass
+
+class Hydra(Monster): pass
+
+class Weapon:
+    def __init__(self, name: str, damage: int) -> None:
+        self.name = name    
+        self.damage = damage
