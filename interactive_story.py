6a82a8ff8f8a378d783d65b958254d94f15394ea
Ryanfrump <ryanfrump@gmail.com>
2024-02-15 11:24:24 -0700

    add welcome message and both paths

--- interactive_story.py
+++ interactive_story.py
@@ -2,6 +2,13 @@ from player import Player
 from monster import Monster
 from weapon import Weapon
 
+name = input("What is your name?")
 
+welcome_message = """Welcome {name}! Welcome to the dream world.
+
+inside here, you will be able to choose 1 of 2 paths.
+You have your path to the left and path to the right...
+
+Which one do you choose?"""
 
 
