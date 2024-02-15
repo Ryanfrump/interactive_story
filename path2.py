6521e1d707b2abe9925fb58dc82fc23cb8fb0021
Hannah Kearl <hannah@Hannahs-MacBook-Air.local>
2024-02-15 11:57:46 -0700

    Added welcome message to path 2

---
+++ path2.py
@@ -0,0 +1,12 @@
+from player import Player
+from monster import Monster
+from weapon import Weapon
+
+intro = """Ahh yes...path 2...the mystery of it is daunting. 
+What will you come across? 
+Or should I say who will you come across? 
+Where are you going? 
+You will soon unlock all of these unknown secrets. 
+All I will say is it's going to be a gaurenteed adventure of a lifetime."""
+print(intro)
+
