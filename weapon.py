228da6edde2f2b5b0d1813bc8861daaf1f1a286f
Hannah Kearl <hannah@Hannahs-MacBook-Air.local>
2024-02-20 20:47:57 -0700

    pushing to git repo

--- weapon.py
+++ weapon.py
@@ -2,4 +2,20 @@ class Weapon:
     def __init__(self, name: str, damage: int) -> None:
         self.name = name    
         self.damage = damage
-        
+
+class Weapon:
+    def __init__(self, name: str, damage: int) -> None:
+        self.name = name
+        self.damage = damage
+
+class Sword(Weapon):
+    def __init__(self) -> None:
+        super().__init__(name="Sword", damage=10)
+
+class Wand(Weapon):
+    def __init__(self) -> None:
+        super().__init__(name="Wand", damage=8)
+
+class Bow(Weapon):
+    def __init__(self) -> None:
+        super().__init__(name="Bow", damage=12        
